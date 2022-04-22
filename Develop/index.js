0dc4891a87932d1b59627460aeed0aacdbc42c8f
DeNiro Dumas <denirod@Deniros-MBP.attlocal.net>
2022-04-22 00:44:25 -0400

    added ReadMe Generator

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,130 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generatePage = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'nameTitle',
+            message: 'What is the title name for the project?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a title')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a brief description?',
+            validate: descripInput => {
+                if (descripInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a title')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Describe the installation instructions?',
+
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide usage information for project',
+
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose the appropriate license',
+            choices: [
+                "ISC",
+                "Apache",
+                "Academic",
+                "GNU",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Is there a test?',
+
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Who are the contributors of this projects?',
+
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Please enter your Github username:',
+            validate: githubName => {
+                if (githubName) {
+                    return true
+                } else {
+                    return false
+                }
+            }
+
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email:'
+
+        }
+    ])
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeFile = res => {
+    fs.writeFile('Gen_README.md', res, err => {
+        if (err) {
+            console.log(err)
+            return;
+        } else {
+            console.log('Your README has been successfully created!')
+        }
+    })
+
+}
+
+questions()
+    .then(answers => {
+        return generatePage(answers);
+    })
+    .then(res => {
+        return writeFile(res);
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() { }
 
 // Function call to initialize app
-init();
+// init();
+var arr = ['apple', 'bannanas', 'peaches', 'strawberries']
+
+const { apple, bannanas, ...rest } = arr
+
+var newArr = [ ...arr, 'kiwi']
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,61 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `![Badge](https://img.shields.io/badge/license-${license}-blue.svg)`
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+    return `https://img.shields.io/badge/license-${license}-blue.svg`
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.nameTitle}
+
+${renderLicenseBadge(data.license)}
+
+${data.description}
+
+## Table of Contents
+
+* [Install](#install)
+* [Usage](#usage)
+* [License](#license)
+* [Test](#test)
+* [Contributors](#contributors)
+* [Questions](#questions)
+
+## Install
+
+${data.install}
+
+## Usage
+${data.usage}
+
+## License
+${data.license}
+
+
+## Test
+${data.test}
+
+
+## Contributors
+${data.contribution}
+
+
+## Questions
+
+If any questions contact me at : ${data.email}
+Github : ${data.username}
+
 
 `;
 }
